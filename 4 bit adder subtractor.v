b69ca9c325c8dbb2ad32901505744cc4fb11f551
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-02 19:59:20 +0600

    Update 4 bit adder subtractor.v

--- 4 bit adder subtractor.v
+++ 4 bit adder subtractor.v
@@ -1,4 +1,5 @@
-module adder_subtractor(s, cout, a, b, mode);
+
+module adder_subtractor(s,cout,a,b,mode);
     input [3:0] a, b;
     input mode;
     output [3:0] s;
@@ -24,4 +25,27 @@ module half_adder(s, cout, a, b);
     output s, cout;
     xor(s, a, b);
     and(cout, a, b);
+endmodule
+module FourAdder_subtractor_tester;
+
+reg [3:0]a,b;
+reg mode;
+wire [3:0]s;
+wire cout;
+
+adder_subtractor four_a_s(s,cout,a,b,mode);
+initial
+begin
+
+         #0  a = 4'b11;   b = 4'b1;    mode = 0; // 4
+        #50 a = 4'b100;  b = 4'b10;   mode = 0; // 6
+        #50 a = 4'b101;  b = 4'b110;  mode = 0; // 11
+        #50 a = 4'b110;  b = 4'b111;  mode = 0; // 13
+        #50 a = 4'b111;  b = 4'b1000; mode = 0; // 15
+        #50 a = 4'b1001; b = 4'b1000; mode = 1; // 1
+        #50 a = 4'b1001; b = 4'b11;   mode = 1; // 6
+        #50 a = 4'b1100; b = 4'b1000; mode = 1; // 4
+        #50 a = 4'b1100; b = 4'b11;   mode  = 1; // 9
+end
+
 endmodule
