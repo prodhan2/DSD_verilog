83b5c906162583ff131fbbcd28f75e18551b71e5
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-02 12:47:41 +0600

    Update 4 bit adder subtractor.v

--- 4 bit adder subtractor.v
+++ 4 bit adder subtractor.v
@@ -1,54 +1,27 @@
-module bcd_adder (sum,cout,a,b);
-
-input [3:0]a,b;
-output [3:0]sum;
-wire w;
-wire [3:0]z;
-output cout;
-assign {w,z} = a+b;
-
-wire c,and_z2_z3,and_z1_z3;
-
-and(and_z1_z3,z[1],z[3]);
-and(and_z2_z3,z[2],z[3]);
-
-or(c,w,and_z2_z3,and_z1_z3);
-
-wire [3:0]m;
-wire [3:0]n;
-
-assign {m} = z; 
-assign n[0] = 0;
-assign n[3] = 0;
-assign n[1] = c;
-assign n[2] = c;
-
-assign {cout,sum} = m+n;
-    
+module adder_subtractor(s, cout, a, b, mode);
+    input [3:0] a, b;
+    input mode;
+    output [3:0] s;
+    output cout;
+    wire c0, c1, c2;
+    full_adder fa1(s[0], c0, a[0], (b[0]^mode), mode);
+    full_adder fa2(s[1], c1, a[1], (b[1]^mode), c0);
+    full_adder fa3(s[2], c2, a[2], (b[2]^mode), c1);
+    full_adder fa4(s[3], cout, a[3], (b[3]^mode), c2);
 endmodule
 
-module bcd_adder_tester;
-reg [3:0]a,b;
-wire [3:0]sum;
-wire cout;
-bcd_adder bcd(sum,cout,a,b);
+module full_adder(s, cout, a, b, cin);
+    input a, b, cin;
+    output s, cout;
+    wire p, q, r;
+    half_adder ha1(p, q, a, b);
+    half_adder ha2(s, r, p, cin);
+    or(cout, q, r);
+endmodule
 
-initial 
-begin
-#0      a = 4'b1000; b = 4'b 0001;
-#100    a = 4'b1000; b = 4'b 0010;
-#100    a = 4'b1000; b = 4'b 0011;
-#100    a = 4'b1000; b = 4'b 0100;
-#100    a = 4'b1000; b = 4'b 0101;
-#100    a = 4'b1000; b = 4'b 0110;
-#100    a = 4'b1000; b = 4'b 0111;
-#100    a = 4'b1000; b = 4'b 1000;
-#100    a = 4'b1000; b = 4'b 1001;
-#100    a = 4'b1000; b = 4'b 1010;
-#100    a = 4'b1000; b = 4'b 1011;
-#100    a = 4'b1000; b = 4'b 1100;
-#100    a = 4'b1000; b = 4'b 1101;
-
-
-end
+module half_adder(s, cout, a, b);
+    input a, b;
+    output s, cout;
+    xor(s, a, b);
+    and(cout, a, b);
 endmodule
