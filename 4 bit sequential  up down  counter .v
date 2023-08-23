2e2264391d08fadf4792c8a9a2261ed69d88e8a7
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-23 11:04:51 +0600

    Update 4 bit sequential  up down  counter .v

--- 4 bit sequential  up down  counter .v
+++ 4 bit sequential  up down  counter .v
@@ -1,34 +1,36 @@
-module masterSlaveff(q,qb,j,k,clr,clk);
-input j,k,clr,clk;
-output q,qb;
-wire a,b,y,z,c,d,cb;
-
-nand(a,qb,j,clk,clr);
-nand(b,q,k,clk);
-nand(y,a,z);
-nand(z,b,y,clr);
-
-not(cb,clk);
-
-nand(c,y,cb);
-nand(d,cb,z);
-nand(q,c,qb);
-nand(qb,q,d,clr);
-
+module masterslavejkff(q, qb, j, k, clr, clk);
+   
+    output q, qb; 
+   input j, k, clr, clk;
+    wire a, b, c, d, e, f;
+    
+    nand(a, j,clk, clr, qb);
+    nand(b, k,clk,  q);
+    nand(c, a, d);
+    nand(d, c, b,clr);
+    
+    wire clkb;
+    not (clkb ,clk); // Invert the clock signal
+    
+    nand(e, clkb, c);
+    nand(f, clkb, d);
+    nand(q, qb, e);
+    nand(qb, q, clr, f);
 endmodule
 
 
-module sequestialCouunterLab(q,clr,clk);
+module sequentialUPdown(q,qb,clr,clk);
+
+output [3:0]q,qb;
 input clr,clk;
-output [3:0]q;
-wire [3:0]qb;
 
-masterSlaveff jk1(q[0],qb[0],q[1],qb[1],clr,clk);
 
-masterSlaveff jk2(q[1],qb[1],q[2],qb[2],clr,clk);
+masterslavejkff f1(q[0],qb[0],q[1],qb[1],clr,clk);
 
-masterSlaveff jk3(q[2],qb[2],q[3],qb[3],clr,clk);
+masterslavejkff f2(q[1],qb[1],q[2],qb[2],clr,clk);
 
-masterSlaveff jk4(q[3],qb[3],qb[0],q[0],clr,clk);
+masterslavejkff f3(q[2],qb[2],q[3],qb[3],clr,clk);
 
-endmodule
+masterslavejkff f4(q[3],qb[3],qb[0],q[0],clr,clk);
+
+endmodule 
